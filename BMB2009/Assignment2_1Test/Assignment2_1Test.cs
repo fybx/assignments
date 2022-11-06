e993a0ffeb41b071b77acc7edcfd36be74b7bf57
Ferit Yiğit BALABAN <f@fybx.dev>
2022-11-06 23:28:05 +0300

    Rename DispCrown => _DispCrown

--- BMB2009/Assignment2_1Test/Assignment2_1Test.cs
+++ BMB2009/Assignment2_1Test/Assignment2_1Test.cs
@@ -13,14 +13,8 @@ public class Tests
     // ReSharper disable once IdentifierTypo
     public void TestDispCrown()
     {
-        Assert.AreEqual("1  1  1" +
-                        "1221221", DispCrown(2));
-        Assert.AreEqual("1    1    1" +
-                        "12  212  21" +
-                        "12332123321", DispCrown(3));
-        Assert.AreEqual("1      1      1" +
-                        "12    212    21" +
-                        "123  32123  321" +
-                        "123443212344321", DispCrown(4));
+        Assert.AreEqual("1  1  1\n1221221", _DispCrown(2));
+        Assert.AreEqual("1    1    1\n12  212  21\n12332123321", _DispCrown(3));
+        Assert.AreEqual("1      1      1\n12    212    21\n123  32123  321\n123443212344321", _DispCrown(4));
     }
 }
