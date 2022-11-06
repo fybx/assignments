27182559b1ba4d71219ca63276f5c02b897a68e8
Ferit Yiğit BALABAN <f@fybx.dev>
2022-11-06 22:38:04 +0300

    Add boilerplate code for Assignment2_1

---
+++ BMB2009/Assignment2_1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Assignment2_1;
+
+// ReSharper disable once InconsistentNaming
+public static class Assignment2_1
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Soru seç (1 veya 2): ");
+        string? input = Console.ReadLine();
+        if (Convert.ToInt32(input) == 1)
+            while (true)
+            {
+                Console.WriteLine("n gir: ");
+                input = Console.ReadLine();
+                if (Convert.ToInt32(input) > 2)
+                {
+                    // DispCrown(Convert.ToInt32(input));
+                }
+            }
+        else if (Convert.ToInt32(input) == 2)
+            while (true)
+            {
+            }
+    }
+}
