f5282da3fad4fcd1251d8d9b3f2d9f24a0cfca1e
Ferit Yiğit BALABAN <f@fybx.dev>
2022-11-06 16:44:20 +0300

    Refactor names to fit it into report

--- BMB2009/Assignment3/Program.cs
+++ BMB2009/Assignment3/Program.cs
@@ -28,46 +28,44 @@ public static class Assignment3
 
 public static class Extensions
 {
-    private static Dictionary<int, string> BasBirler = new()
+    private static Dictionary<int, string> BasBir = new()
     {
         {1, "bir"}, {2, "iki"}, {3, "üç"}, {4, "dört"}, {5, "beş"}, {6, "altı"}, {7, "yedi"}, {8, "sekiz"}, {9, "dokuz"}
     };
     
-    private static Dictionary<int, string> BasOnlar = new()
+    private static Dictionary<int, string> BasOn = new()
     {
         {1, "on"}, {2, "yirmi"}, {3, "otuz"}, {4, "kırk"}, {5, "elli"}, {6, "altmış"}, {7, "yetmiş"}, {8, "seksen"}, {9, "doksan"}
     };
 
-    private static Dictionary<int, string> UcluGrup = new()
+    private static Dictionary<int, string> Binlik = new()
     {
         { 0, "" }, { 1, "bin" }, { 2, "milyon" }, { 3, "milyar" }
     };
 
     public static string Oku(this int deger)
     {
-        int ucl = 0;
-        int rakam = 0;
-        string metin = "";
+        int bin = 0, dig = 0, peek = 0, dgr = deger;
+        string txt = "";
         do
         {
             int pos = 0;
-            metin = $"{(UcluGrup[ucl])} {metin.Trim()}";
+            txt = bin == 0 ? "" : $"{(Binlik[bin])} {txt.Trim()}";
             do
             {
-                rakam = deger % 10;
-                metin = pos switch
+                dig = dgr % 10;
+                peek = (dgr / 10) % 10;
+                txt = pos switch
                 {
-                    0 => $"{(ucl == 1 && rakam == 1 && ((deger / 10) % 10) == 0 ? string.Empty : BasBirler[rakam])} {metin}".Trim(),
-                    1 => $"{BasOnlar[rakam]} {metin}",
-                    _ => $"{(rakam == 1 ? string.Empty : BasBirler[rakam])} yüz {metin}"
+                    0 => $"{(bin == 1 && pos == 1 && peek == 0 ? "" : BasBir[dig])} {txt}".Trim(),
+                    1 => $"{BasOn[dig]} {txt}",
+                    _ => $"{(dig == 1 ? "" : BasBir[dig])} yüz {txt}"
                 };
-                deger /= 10;
+                dgr /= 10;
                 pos++;
-            } while (pos < 3 && deger != 0);
-            
-            ucl++;
-        } while (deger != 0);
-
-        return metin.TrimStart();
+            } while (pos < 3 && dgr != 0);
+            bin++;
+        } while (dgr != 0);
+        return txt.Trim();
     }
 }
