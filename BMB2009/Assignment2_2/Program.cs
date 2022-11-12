3d7e3ffdb50cec31ed00d9b0b58379a7731f0502
Ferit Yiğit BALABAN <f@fybx.dev>
2022-11-12 23:25:30 +0300

    Create new project Assignment2_2

---
+++ BMB2009/Assignment2_2/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ *      Ferit Yiğit BALABAN, <f@fybx.dev>
+ *      032190002
+ */
+
+using System;
+
+namespace Assignment2_2;
+
+// ReSharper disable once InconsistentNaming
+public static class Assignment2_2
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, world!");   
+    }
+}
