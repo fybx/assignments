f9e55856e135e778eb0992e4d9c8393c0bba6a77
Ferit Yiğit BALABAN <f@fybx.dev>
2022-11-13 00:06:12 +0300

    Introduce public overrides for unit testing with extended argument list

--- BMB2009/Assignment2_2/Program.cs
+++ BMB2009/Assignment2_2/Program.cs
@@ -50,17 +50,17 @@ public static class Assignment2_2
     /// Does collision checks against console window borders and itself.
     /// </summary>
     /// <returns>Returns true if collision occurs, otherwise false.</returns>
-    public static bool CheckCollision()
+    private static bool CheckCollision()
     {
         throw new NotImplementedException();
     }
-    
+
     /// <summary>
     /// Calculates new coordinates of snake's body parts.
     /// </summary>
     /// <param name="direction">Direction player stepped to; up being 1, right 2, down 3 and left 4.</param>
     /// <returns>Returns updated coordinate array of body.</returns>
-    public static int[,] CalculateSnakeBody(int direction)
+    private static int[,] CalculateSnakeBody(int direction)
     {
         throw new NotImplementedException();
     }
@@ -70,10 +70,20 @@ public static class Assignment2_2
     /// </summary>
     /// <param name="state">Current game state</param>
     /// <param name="draw">True by default, can be used to skip states</param>
-    public static void Redraw(int state, bool draw = true)
+    private static void Redraw(int state, bool draw = true)
     {
         if (!draw)
             return;
         throw new NotImplementedException();
+    }
+
+    public static bool CheckCollision(int[,] snakeBody)
+    {
+        return CheckCollision();
+    }
+
+    public static int[,] CalculateSnakeBody(int[,] snakeBody, int direction)
+    {
+        return CalculateSnakeBody(direction);
     }
 }
