f938c2838a2b54cb54fda9f0c26974dfd5ea7841
Ferit Yiğit BALABAN <f@fybx.dev>
2022-11-13 00:03:26 +0300

    Create unit test for Assignment2_2 methods

---
+++ BMB2009/Assignment2_2Test/Assignment2_2Test.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+
+namespace Assignment2_2Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void TestCheckCollision()
+    {
+        Assert.Pass();
+    }
+
+    public void TestCalculateSnakeBody()
+    {
+        Assert.Pass();
+    }
+}
