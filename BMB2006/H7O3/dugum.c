1ee360bfbf085627fd26894e7c46a39a75b547a0
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-04-11 00:16:04 +0300

    base functionality for ikilidugum

--- BMB2006/H7O3/dugum.c
+++ BMB2006/H7O3/dugum.c
@@ -9,7 +9,7 @@
 #include "dugum.h"
 
 Dugum* yeni_dugum(int deger) {
-    Dugum *d    = (Dugum *) calloc(1, sizeof(Dugum));
+    Dugum* d    = (Dugum*)calloc(1, sizeof(Dugum));
     d->deger    = deger;
     d->sonraki  = NULL;
     return d;
@@ -20,5 +20,21 @@ int dugum_guncelle(Dugum* dugum, int deger) {
         return -1;
 
     dugum->deger = deger;
-    return deger;
+    return 0;
+}
+
+IkiliDugum* yeni_ikilidugum(int deger) {
+    IkiliDugum* d   = (IkiliDugum*)calloc(1, sizeof(IkiliDugum));
+    d->deger        = deger;
+    d->sag          = NULL;
+    d->sol          = NULL;
+    return d;
+}
+
+int ikilidugum_guncelle(IkiliDugum* dugum, int deger) {
+    if (!dugum)
+        return -1;
+
+    dugum->deger = deger;
+    return 0;
 }
